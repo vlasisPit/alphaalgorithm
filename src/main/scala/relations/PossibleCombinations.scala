f24a2a4df8a235982c40ba5e9c56e428171fcd90
Vlasis Pitsios <vlassisp89@gmail.com>
2018-09-16 20:29:34 +0300

    Find all possible group combinations for an event set (not working yet)

---
+++ src/main/scala/relations/PossibleCombinations.scala
@@ -0,0 +1,45 @@
+package relations
+
+/**
+  * Find all possible sub-groups for a given data set
+  * @param events
+  * @tparam T
+  */
+class PossibleCombinations[T](val events: List[T]) {
+
+  def extractAllPossibleCombinations(): List[Set[T]] = {
+    var groups : List[Set[T]] = List()
+
+    for( i <- 0 to scala.math.pow(2,events.size).toInt-1 ){
+      val counterBinary = convertToBinary(i, events.size)
+
+      var group : Set[T] = Set()
+      for (j <- 0 to (counterBinary.length-1)) {
+        if (counterBinary.charAt(j) == '1') {
+          group = group + events(j)
+        }
+      }
+      groups = group :: groups
+
+    }
+
+    return groups
+  }
+
+  def convertToBinary(i: Int, targetLength: Int) : String = {
+    val num : String = i.toBinaryString
+
+    if (num.length==targetLength) {
+      return num
+    } else {
+      val numberOfZerosToAdd = targetLength-num.length
+      var zeros = ""
+      for (i <- 0 to numberOfZerosToAdd-1) {
+        zeros = zeros + "0"
+      }
+
+      return zeros + num
+    }
+  }
+
+}
---
+++ src/test/scala/relations/PossibleCombinationsTest.scala
@@ -0,0 +1,54 @@
+package relations
+
+import org.scalatest.FunSuite
+
+class PossibleCombinationsTest extends FunSuite {
+
+  test("Extract possible combinations from 3 member list") {
+    val groupEvents : List[String] = List("B", "C", "E")
+
+    val possibleCombinations : PossibleCombinations[String] = new PossibleCombinations(groupEvents);
+    val combinations : List[Set[String]] = possibleCombinations.extractAllPossibleCombinations();
+
+    assert(combinations.contains(Set()))
+    assert(combinations.contains(Set("E")))
+    assert(combinations.contains(Set("C")))
+    assert(combinations.contains(Set("C","E")))
+    assert(combinations.contains(Set("B")))
+    assert(combinations.contains(Set("B","E")))
+    assert(combinations.contains(Set("B","C")))
+    assert(combinations.contains(Set("B","C","E")))
+
+    assert(combinations.length==8)
+  }
+
+  test("Extract possible combinations from 4 member list") {
+    val groupEvents : List[String] = List("B", "C", "D", "E")
+
+    val possibleCombinations : PossibleCombinations[String] = new PossibleCombinations(groupEvents);
+    val combinations : List[Set[String]] = possibleCombinations.extractAllPossibleCombinations();
+
+    assert(combinations.contains(Set()))
+    assert(combinations.contains(Set("E")))
+    assert(combinations.contains(Set("D")))
+    assert(combinations.contains(Set("D","E")))
+
+    assert(combinations.contains(Set("C")))
+    assert(combinations.contains(Set("C","E")))
+    assert(combinations.contains(Set("C","D")))
+    assert(combinations.contains(Set("C","D","E")))
+
+    assert(combinations.contains(Set("B")))
+    assert(combinations.contains(Set("B","E")))
+    assert(combinations.contains(Set("B","D")))
+    assert(combinations.contains(Set("B","D","E")))
+
+    assert(combinations.contains(Set("B","C")))
+    assert(combinations.contains(Set("B","C","E")))
+    assert(combinations.contains(Set("B","C","D")))
+    assert(combinations.contains(Set("B","C","D","E")))
+
+    assert(combinations.length==16)
+  }
+
+}
