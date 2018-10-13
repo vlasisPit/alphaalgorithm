fde92963f837f1fb9c093f81429711645b73b633
Vlasis Pitsios <vlassisp89@gmail.com>
2018-10-13 20:21:04 +0300

    Refactor FindFollowRelation procedure

--- src/main/scala/steps/FindFollowRelation.scala
+++ src/main/scala/steps/FindFollowRelation.scala
@@ -19,23 +19,28 @@ class FindFollowRelation() extends Serializable {
   def findFollowRelation(traceWithCaseId: (String, List[String]), pairsToExamine: List[String]):FullPairsInfoMap = {
     var pairInfoMap = pairInfoInit(pairsToExamine)
 
-    for( i <- 0 to traceWithCaseId._2.length-2){
-      var pair : String = traceWithCaseId._2(i)+traceWithCaseId._2(i+1)
-      val pairExists : Boolean = checkIfPairExists(pair, pairInfoMap)
-      if (pairExists == false) {
-        //reverse relation in this case
-        pair = pair.reverse
-        val tuple2 = pairInfoMap(pair)
-        val newTuple2 = (tuple2._1,new PairNotation(Directionality.INVERSE, Relation.FOLLOW))
-        pairInfoMap = pairInfoMap + (pair -> newTuple2)
-      } else {
-        val tuple2 = pairInfoMap(pair)
-        val newTuple2 = (new PairNotation((Directionality.DIRECT, Relation.FOLLOW)), tuple2._2)
-        pairInfoMap = pairInfoMap + (pair -> newTuple2)
-      }
-    }
+    Range(0, traceWithCaseId._2.length-1)
+      .map(i=> traceWithCaseId._2(i)+traceWithCaseId._2(i+1))
+      .map(pair=> {
+          val pairExists : Boolean = checkIfPairExists(pair, pairInfoMap)
+          pairInfoMap = matchPairExists(pairExists, pair, pairInfoMap)
+        }
+      )
 
     return new FullPairsInfoMap(pairInfoMap)
+  }
+
+  def matchPairExists(bool: Boolean, pair: String, pairInfoMap : Map[String, (PairNotation, PairNotation)]): Map[String, (PairNotation, PairNotation)] = bool match {
+    case false => {//reverse relation in this case
+        val tuple2 = pairInfoMap(pair.reverse)
+        val newTuple2 = (tuple2._1,new PairNotation(Directionality.INVERSE, Relation.FOLLOW))
+        pairInfoMap + (pair.reverse -> newTuple2)
+      }
+    case true => {
+        val tuple2 = pairInfoMap(pair)
+        val newTuple2 = (new PairNotation((Directionality.DIRECT, Relation.FOLLOW)), tuple2._2)
+        pairInfoMap + (pair -> newTuple2)
+      }
   }
 
   def checkIfPairExists(pair: String, pairInfoMap: Map[String, (PairNotation, PairNotation)]) : Boolean = {
@@ -48,12 +53,9 @@ class FindFollowRelation() extends Serializable {
     * @return
     */
   private def pairInfoInit(pairsToExamine: List[String]) : Map[String, (PairNotation, PairNotation)] = {
-    var pairInfo: Map[String, (PairNotation, PairNotation)] = Map()
-
-    for(i <- 0 to pairsToExamine.length-1) {
-      pairInfo = pairInfo + (pairsToExamine(i) -> (new PairNotation((Directionality.DIRECT, Relation.NOT_FOLLOW)), new PairNotation((Directionality.INVERSE, Relation.NOT_FOLLOW))))
-    }
-    return pairInfo
+    pairsToExamine
+      .map(pair => pair -> (new PairNotation((Directionality.DIRECT, Relation.NOT_FOLLOW)), new PairNotation((Directionality.INVERSE, Relation.NOT_FOLLOW))))
+      .toMap
   }
 
 }
