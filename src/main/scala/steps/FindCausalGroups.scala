5a36cae17d927eea0cb50cb5dc1ba91be471612a
Vlasis Pitsios <vlassisp89@gmail.com>
2018-12-27 18:38:08 +0200

    Back to Second solution for causal groups (its slower but more accurate)

--- src/main/scala/steps/FindCausalGroups.scala
+++ src/main/scala/steps/FindCausalGroups.scala
@@ -1,7 +1,5 @@
 package steps
 
-import java.util.concurrent.TimeUnit
-
 import misc.{CausalGroup, Pair, Relation}
 import org.apache.spark.sql.{Dataset, Encoder, Encoders, SparkSession}
 
@@ -21,28 +19,26 @@ import org.apache.spark.sql.{Dataset, Encoder, Encoders, SparkSession}
   * to each element of R (ie all pairwise combinations of elements of Q and R are in ->) and the members of Q and R are
   * not in ||
   *
-  * First of all, algorithm finds all causal relations by filtering these data
-  * Secondly, algorithm computes causal groups from the first members to the second trying to make groups of the same
-  * members.
-  * Same, for the other side
-  * @tparam T
+  * The algorithm works as follows:
+  * Lets say that we have the causal relations
+  * a->d
+  * a->c
+  * b->d
+  * b->c
+  * and the following never follow relation
+  * a#b
+  * c#d
+  * 1) Find all events from both sides (left and right) {a,b} and {c,d}
+  * 2) Find all possible combination for each set (left and right) List[{a,b} {a} {b}] and List[{c,d} {c} {d}]
+  * 3) Delete from above lists all the sets which are not in never follow relation with each other
+  * 4) Connect all the sets (as causal groups) from two lists and keep only those for which all events are in causal relations
   */
 @SerialVersionUID(100L)
 class FindCausalGroups(val logRelations: Dataset[(Pair, String)]) extends Serializable {
   implicit def pairEncoder: org.apache.spark.sql.Encoder[Pair] = org.apache.spark.sql.Encoders.kryo[Pair]
-  implicit def causalGroupGenericEncoder: org.apache.spark.sql.Encoder[CausalGroup[String]] = org.apache.spark.sql.Encoders.kryo[CausalGroup[String]]
-  implicit def setEncoder: org.apache.spark.sql.Encoder[Set[String]] = org.apache.spark.sql.Encoders.kryo[Set[String]]
-  //implicit def tupleEncoder: org.apache.spark.sql.Encoder[String] = org.apache.spark.sql.Encoders.kryo[String]
-  implicit def pairStringEncoder: org.apache.spark.sql.Encoder[(Pair, String)] = org.apache.spark.sql.Encoders.kryo[(Pair, String)]
-  implicit def listCausalGroupsEncoder: org.apache.spark.sql.Encoder[List[CausalGroup[String]]] = org.apache.spark.sql.Encoders.kryo[List[CausalGroup[String]]]
-  implicit def tuple2[A1, A2](
-                               implicit e1: Encoder[A1],
-                               e2: Encoder[A2]
-                             ): Encoder[(A1,A2)] = Encoders.tuple[A1,A2](e1, e2)
 
   val spark = SparkSession.builder().getOrCreate()
 
-  val t0 = System.nanoTime()
   val never = logRelations
     .filter(x=>x._2==Relation.NEVER_FOLLOW.toString)
     .map(neverRelation => neverRelation._1)
@@ -51,88 +47,74 @@ class FindCausalGroups(val logRelations: Dataset[(Pair, String)]) extends Serial
     .toList
 
   val neverBc = spark.sparkContext.broadcast(never)
-  println("Collect to list " + TimeUnit.SECONDS.convert(System.nanoTime() - t0, TimeUnit.NANOSECONDS) + "sec")
+
+  val causal = logRelations
+    .filter(x=>x._2==Relation.CAUSALITY.toString)
+    .map(neverRelation => neverRelation._1)
+    .distinct()
+    .collect()
+    .toList
+
+  val causalBc = spark.sparkContext.broadcast(causal)
 
   def extractCausalGroups():Dataset[CausalGroup[String]] = {
-    val directCausalGroups = logRelations
+    implicit def stringEncoder: org.apache.spark.sql.Encoder[String] = org.apache.spark.sql.Encoders.kryo[String]
+    implicit def tupleStringEncoder: org.apache.spark.sql.Encoder[List[(String, Set[String])]] = org.apache.spark.sql.Encoders.kryo[List[(String, Set[String])]]
+    implicit def tupleListStringEncoder: org.apache.spark.sql.Encoder[(String, List[Set[String]])] = org.apache.spark.sql.Encoders.kryo[(String, List[Set[String]])]
+    implicit def strEncoder: org.apache.spark.sql.Encoder[List[Set[String]]] = org.apache.spark.sql.Encoders.kryo[List[Set[String]]]
+    implicit def gEncoder: org.apache.spark.sql.Encoder[(String, Set[String])] = org.apache.spark.sql.Encoders.kryo[(String, Set[String])]
+    implicit def causalGroupGenericEncoder: org.apache.spark.sql.Encoder[CausalGroup[String]] = org.apache.spark.sql.Encoders.kryo[CausalGroup[String]]
+
+    implicit def tuple2[A1, A2](
+                                 implicit e1: Encoder[A1],
+                                 e2: Encoder[A2]
+                               ): Encoder[(A1,A2)] = Encoders.tuple[A1,A2](e1, e2)
+
+    val directCausalRelations = logRelations
       .filter(x=>x._2==Relation.CAUSALITY.toString)
       .map(x=>x._1)
-      .map(x=>new CausalGroup(Set(x.member1), Set(x.member2)))
-      .distinct()
+      .map(x=>(x.member1, x.member2))
 
-    val causalGroupsFromLeftPart = directCausalGroups
-      .groupByKey(x=>x.getFirstGroup())
-      .mapGroups{case(k, iter) => (k, iter.map(x => x.getSecondGroup().head).toSet)}
-      .filter(x=>x._2.size>1)
-      .map(x=>new CausalGroup(x._1, x._2))
-      .distinct()
-      .collect()
-      .toList
+    //1) Find all events from both sides (left and right) {a,b} and {c,d}
+    val uniqueEventsFromLeftSideEvents = directCausalRelations
+      .map(causal => ("left", causal._1))
+      .groupByKey(x=>x._1)
+      .mapGroups{case(k, iter) => (k, iter.map(x => x._2).toSet)}
 
-    val causalGroupsFromLeft = causalGroupsFromLeftPart
-      .flatMap(x=> createFinalCausalGroupsLeft(x))
+    val uniqueEventsFromRightSideEvents = directCausalRelations
+      .map(causal => ("right", causal._2))
+      .groupByKey(x=>x._1)
+      .mapGroups{case(k, iter) => (k, iter.map(x => x._2).toSet)}
 
-    val causalGroupsFromRightPart = directCausalGroups
-      .map(x=>new CausalGroup(x.getSecondGroup(), x.getFirstGroup()))
-      .groupByKey(x=>x.getFirstGroup())
-      .mapGroups{case(k, iter) => (k, iter.map(x => x.getSecondGroup().head).toSet)}
-      .filter(x=>x._2.size>1)
-      .map(x=>new CausalGroup(x._1, x._2))
-      .distinct()
-      .collect()
-      .toList
-
-    val causalGroupsFromRight = causalGroupsFromRightPart
-      .flatMap(x=> createFinalCausalGroupsRight(x))
+    //2) Find all possible combination for each set (left and right) List[{a,b} {a} {b}] and List[{c,d} {c} {d}]
+    //3) Delete from above lists all the sets which are not in never follow relation with each other
+    val groups = uniqueEventsFromLeftSideEvents
+      .union(uniqueEventsFromRightSideEvents)
+      .flatMap(uniqueEvents => possibleSubsets(uniqueEvents))
+      .filter(subset=> allRelationsAreNeverFollow(subset._2))
+      .groupByKey(x=>x._1)
+      .mapGroups{case(k, iter) => (k, iter.map(x => x._2).toList)}
 
     import spark.implicits._
-    return directCausalGroups
-      .union(causalGroupsFromLeft.toDS())
-      .union(causalGroupsFromRight.toDS());
+    val leftGroups = groups.first()
+    val rigthGroups = groups.orderBy($"value".desc).first()
+
+    //4) Connect all the sets (as causal groups) from two lists and keep only those for which all events are in causal relations
+    val causalGroups = leftGroups._2.toDS().as("left")
+      .crossJoin(rigthGroups._2.toDS().as("right"))
+      .where($"left.value" !== $"right.value")
+      .toDF("left", "right")
+      .filter(group=>isCausalRelationValid(group.getAs[Seq[String]]("left").toSet, group.getAs[Seq[String]]("right").toSet))
+      .map(group=>new CausalGroup(group.getAs[Seq[String]]("left").toSet,group.getAs[Seq[String]]("right").toSet))
+
+    causalGroups
   }
 
-  /**
-    * Input a causal group which we have to inspect for breaking to more groups,
-    * if NeverFollow relation is not valid
-    * Example causal group {a} -> {b,c,e}
-    * to {a} -> {b,e} and {a} -> {c,e}
-    */
-  def createFinalCausalGroupsLeft(causalGroup: CausalGroup[String]): List[CausalGroup[String]] = {
-    return checkIfNeverFollowRelationIsValidAndBreakTheGroup(causalGroup.getSecondGroup())
-      .map(x => new CausalGroup(causalGroup.getFirstGroup(), x))
-  }
-
-  /**
-    * Input a causal group which we have to inspect for breaking to more groups,
-    * if NeverFollow relation is not valid
-    * Example causal group {b,c,e} ->  {a}
-    * to {b,e} -> {d} and {c,e} -> {d}
-    */
-  def createFinalCausalGroupsRight(causalGroup: CausalGroup[String]): List[CausalGroup[String]] = {
-    return checkIfNeverFollowRelationIsValidAndBreakTheGroup(causalGroup.getSecondGroup())
-      .map(x => new CausalGroup(x, causalGroup.getFirstGroup()))
-  }
-
-  /**
-    * From causal groups provided, we must check if the relation NEVER_FOLLOW is valid.
-    * If not the causal group must be broken to more groups.
-    * For example suppose we have a causal group
-    * {a} -> {b,c,e}
-    * The next step is to check if for all events in {b,c,e}, all events relations are NEVER_FOLLOW.
-    * If yes, {b,c,e} stay as is.
-    * If not the algorithm must break down the set in more sets for which NEVER_FOLLOW is valid.
-    *
-    * @param causalGroupNotCompleted
-    */
-  def checkIfNeverFollowRelationIsValidAndBreakTheGroup(events: Set[String]): List[Set[String]] = {
-    //eg {b,c,e}. Maybe these events are connected with some not NOT-FOLLOW relation, so the group must be broken
-    val possibleCombinations : PossibleCombinations = new PossibleCombinations(events.toList)
-    val allPossibleCombinations = possibleCombinations.extractAllPossibleCombinations()
-    val groups = allPossibleCombinations
-      .filter(x=>allRelationsAreNeverFollow(x))
-      .filter(x=>x.size>1)
-
-    return groups
+  def possibleSubsets(uniqueEvents: (String, Set[String])): List[(String, Set[String])] = {
+    val possibleCombinations : PossibleCombinations = new PossibleCombinations(uniqueEvents._2.toList)
+    val combinations : List[Set[String]] = possibleCombinations.extractAllPossibleCombinations()
+    combinations
+      .map(comb=>(uniqueEvents._1, comb))
   }
 
   /**
@@ -158,19 +140,36 @@ class FindCausalGroups(val logRelations: Dataset[(Pair, String)]) extends Serial
     return new Pair(pair.getSecondMember(), pair.getFirstMember())
   }
 
-/*  def isNeverFollow(pair: Pair): Boolean = {
-    println("check ...")
+  @Deprecated
+  def computeCausalGroups(causalGroupFromLeftSide: List[Set[String]], causalGroupFromRightSide: List[Set[String]]): List[CausalGroup[String]] = {
+    for {
+      groupA <- causalGroupFromLeftSide
+      groupB <- causalGroupFromRightSide
+      if ( (!groupA.isEmpty && !groupB.isEmpty) && (groupA != groupB) && isCausalRelationValid(groupA, groupB))
+    } yield new CausalGroup(groupA,groupB)
+  }
 
-    val t0 = System.nanoTime()
-    val ckeckedPair = neverBc.value
-      .filter(neverPair=>(neverPair.getFirstMember()==pair.getFirstMember() && neverPair.getSecondMember()==pair.getSecondMember())
-        || (neverPair.getFirstMember()==pair.getSecondMember() &&neverPair.getSecondMember()==pair.getFirstMember()))
-      .limit(1)
+  def isCausalRelationValid(groupA: Set[String], groupB: Set[String]): Boolean = {
+    val flags = for {
+      grA <- groupA
+      grB <- groupB
+      if ( (!grA.isEmpty && !grB.isEmpty) && (grA != grB))
+    } yield allEventsAreInCausalityRelation(groupA, groupB)
 
-    val t1 = System.nanoTime()
-    println("Elapsed time compute never follow each: All possible comb " + TimeUnit.SECONDS.convert(t1 - t0, TimeUnit.NANOSECONDS) + "sec")
+    return flags.filter(flag => flag==false).isEmpty
+  }
 
-    ckeckedPair.count() != 0;
-  }*/
+  def allEventsAreInCausalityRelation(groupA: Set[String], groupB: Set[String]): Boolean = {
+    val pairs = for {
+      eventA <- groupA
+      eventB <- groupB
+    } yield new Pair(eventA, eventB)
+
+    for {
+      pair <- pairs
+    } yield if (!causalBc.value.contains(pair)) { return false }
+
+    true
+  }
 
 }
